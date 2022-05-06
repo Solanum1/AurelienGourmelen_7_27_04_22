2c39d72f5e826de9cc42bc0d184fda67187a441d
Aurélien Gourmelen <aurelien.gourmelen@gmail.com>
2022-05-06 16:48:20 +0200

    modification de la fonction signup - ajout de la vérification de l'adresse mail dans la bdd

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -7,17 +7,30 @@ const dotenv = require("dotenv").config();
 //----------------Logique métier--------------------------------
 
 exports.signup = (req, res, next) => {
-    bcrypt
-        .hash(req.body.password, 10)
-        .then((hash) => {
-            models.User.create({
-                email: req.body.email,
-                username: req.body.username,
-                password: hash,
-                isAdmin: 0,
-            });
+    let emailReq = req.body.email;
+    //méthode findOne/where de Sequelize
+    models.User.findOne({
+        where: { email: emailReq },
+    })
+        //vérification que l'adresse mail n'est pas déjà dans la bdd
+        .then((userFound) => {
+            if (!userFound) {
+                bcrypt.hash(req.body.password, 10).then((hash) => {
+                    models.User.create({
+                        email: req.body.email,
+                        username: req.body.username,
+                        password: hash,
+                        isAdmin: 0,
+                    }).then(() => {
+                        res.status(201).json({ message: "Utilisateur créé" });
+                    });
+                });
+            } else {
+                res.status(403).json({
+                    error: "Cette adresse mail existe déjà, merci de choisir une autre adresse mail",
+                });
+            }
         })
-        .then(() => res.status(201).json({ message: "Utilisateur créé" }))
         .catch((error) => {
             res.status(500).json({ error: error.message });
         });
