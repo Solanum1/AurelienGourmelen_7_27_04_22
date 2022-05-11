5bed825a89cd911d3d62d4c35bb8f403873af7d3
Aurélien Gourmelen <aurelien.gourmelen@gmail.com>
2022-05-11 12:03:35 +0200

    implémentation app routing module et route register

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing-module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,6 +16,7 @@ import { RegisterFormComponent } from './register-form/register-form.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule
   ],
   providers: [],
