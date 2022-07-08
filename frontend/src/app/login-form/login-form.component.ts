19a98de122debbfaa9aac38c388823131f9c0af3
Aurélien Gourmelen <aurelien.gourmelen@gmail.com>
2022-07-08 10:07:02 +0200

    reload du header après connexion/deconnexion

--- frontend/src/app/login-form/login-form.component.ts
+++ frontend/src/app/login-form/login-form.component.ts
@@ -42,6 +42,8 @@ export class LoginFormComponent {
       });
     }
     
-    ngOnInit(): void {}
+    ngOnInit(): void {
+      
+    }
 
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -20,6 +20,7 @@ export class AuthService {
     constructor(private http: HttpClient,
                 private router: Router) 
                 {}
+
     
     public register(userData: any): Observable<any> {
         const URI = this.uriseg + '/signup';
